bfd938a84304f701d98e444ba41949b146ccab39
Helloyunho <yunho050840@gmail.com>
2022-02-12 19:58:37 +0900

    💩 feat: try to make navbar but it doesnt work properly

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,15 @@
 
 import { h, html, Page, ssr, tw } from "./common.ts";
 import { examples, parseExample } from "../examples/mod.ts";
+import NavBar from "../components/navbar.tsx";
 
 let cached: string | null;
 
 function Index() {
   return ssr(() => [
     <title>Harmony by Example</title>,
-    <div class={tw`bg-white flex h-screen`}>
+    <div class={tw`bg-white flex h-screen flex-column`}>
+      <NavBar />
       <h1 class={tw`text-5xl text-gray-600 m-auto mt-20`}>
         Harmony by Example
       </h1>
